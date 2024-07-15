4f46cf62b57bcd2bf8ab71bb07691bf255056c93
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2024-07-14 20:48:19 -0500

    export names differently

---
+++ compynators/__init__.py
@@ -0,0 +1,5 @@
+from compynators.compynators import ParseResult, ParseResultType
+from compynators.bools import TrueType, FalseType
+from compynators.span import Span, Spanned
+from compynators.stream import Stream
+from compynators.union import Result, ResultType, Maybe, MaybeType
