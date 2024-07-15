e7cf8cfeef51cc1e40f5376fc6b9f1d31b280770
bwburnsides <19192452+bwburnsides@users.noreply.github.com>
2024-07-14 21:03:48 -0500

    messing with imports

--- compynators/__init__.py
+++ compynators/__init__.py
@@ -1,23 +1,3 @@
-from combinators import ParseResult, ParseResultType
-from bools import TrueType, FalseType
-from span import Span, Spanned
-from stream import Stream
-from union import Result, ResultType, Maybe, MaybeType
-
 import combinators, bools, span, stream, union
 
 _ = (combinators, bools, span, stream, union)
-
-_ = (
-    ParseResult,
-    ParseResultType,
-    TrueType,
-    FalseType,
-    Span,
-    Spanned,
-    Stream,
-    Result,
-    ResultType,
-    Maybe,
-    MaybeType,
-)
